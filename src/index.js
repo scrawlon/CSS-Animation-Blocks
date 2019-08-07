59ed3aeacd2e3f6681246dca7a0e669dd18fb779
Scott McGrath <scrawlon@gmail.com>
2019-08-06 21:27:44 -0400

    keyframeProps

---
+++ src/index.js
@@ -0,0 +1,108 @@
+
+function AnimationBlock(timeline, config) {
+  this.timeline = timeline;
+  this.config = config;
+  this.processedTimeline = function() {
+    let output = {};
+
+    for ( const timeString in timeline ) {
+      const time = timeString.split(':');
+      const minutes = Math.floor(time[0] * 60000);
+      const milliseconds = Math.floor(time[1].replace('.', ''));
+
+      output[minutes + milliseconds] = timeline[timeString];
+    }
+
+    return output;
+  };
+}
+
+AnimationBlock.prototype.start = function() {
+  const styleSheets = document.styleSheets;
+  const timeline = this.processedTimeline();
+  const animationTimes = Object.keys(timeline);
+  const lastAnimationIndex = animationTimes.length - 1;
+  let nextAnimationIndex = 0;
+  let startTime;
+
+  console.log({timeline});
+
+  requestAnimationFrame(animation);
+
+  function animation(timestamp) {
+    if (!startTime) startTime = timestamp;
+
+    const elapsedTime = timestamp - startTime;
+    let dom = {};
+    let keyframeProps = {};
+
+    if ( elapsedTime >= animationTimes[nextAnimationIndex] ) {
+      const animations = timeline[animationTimes[nextAnimationIndex]].animations;
+
+      animations.forEach((animation) => {
+        const { elementSelector, animationClass } = animation;
+
+        if ( !elementSelector ) return false;
+
+        if ( !dom[elementSelector] ) {
+          dom[elementSelector] = {};
+        }
+
+        dom[elementSelector].elements = document.querySelectorAll(elementSelector);
+
+        dom[elementSelector].elements.forEach((element) => {
+          if ( animation.animationClass ) {
+            element.classList.add(animationClass);
+
+            element.addEventListener('animationstart', (event) => {
+              if ( !keyframeProps[event.animationName] ) {
+                keyframeProps[event.animationName] = getKeyframeProps(styleSheets, event.animationName);
+              }
+            });
+
+            element.addEventListener('animationend', (event) => {
+              const endStyles = window.getComputedStyle(element);
+              let styles = '';
+
+              console.log({endStyles});
+
+              keyframeProps[event.animationName].forEach((style) => {
+                styles += `${style}:${endStyles[style]}`;
+              });
+
+              element.style.cssText = styles;
+              element.classList.remove(animationClass);
+            });
+          }
+        });
+      });
+
+      nextAnimationIndex++;
+    }
+
+    if ( nextAnimationIndex <= lastAnimationIndex ) {
+      requestAnimationFrame(animation);
+    }
+  }
+}
+
+function getKeyframeProps(styleSheets, rule) {
+  let props = [];
+
+  for ( let styleSheet of styleSheets ) {
+    const { rules } = styleSheet;
+
+    for ( let rule of rules ) {
+      if ( rule.type === 7 ) {
+        const { cssText } = rule;
+        const cssProps = [...new Set(cssText.match(/\w+(?=:)/g))];
+
+        props = props.concat(cssProps);
+      }
+    }
+  }
+
+  return props;
+}
+
+export { AnimationBlock };
